f1a0217df28bcd985691b7e7c32509cad293be45
Ties Spit <tiesspit@gmail.com>
2026-02-20 16:05:24 +0100

    Added random door generation and some extra controls
    - Space executes next step
    - Escape resets generation

--- Algorithms_Project/Assets/Scripts/DungeonGenerator.cs
+++ Algorithms_Project/Assets/Scripts/DungeonGenerator.cs
@@ -8,47 +8,58 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private RectInt room = new RectInt(0, 0, 100, 50);
 
     [SerializeField] private List<RectInt> toDo = new();
-
     [SerializeField] private List<RectInt> generatedRooms = new();
+    [SerializeField] private List<RectInt> doors = new();
 
-
+    [Header("Room generation")]
     [SerializeField] private int minRoomSize = 10;
-    private int expectedRoomSize = 20;
+    [SerializeField] private int MaxRoomSize;
+    [SerializeField] private int roomOverlap;
 
-    [SerializeField] private bool widthSplit;
+    private bool widthSplit;
+
+    [Header("Door generation")]
+    [SerializeField] private int doorOffset;
+    [SerializeField] private int doorWidth;
 
     private void Start()
     {
         toDo.Add(room);
+        StartGenerate();
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StopAllCoroutines();
+            StartGenerate();
+        }
 
     }
 
-    [ContextMenu("Generate Dungeon")]
+    //[ContextMenu("Generate Dungeon")]
     public void StartGenerate()
     {
+        toDo.Clear();
+        generatedRooms.Clear();
+        doors.Clear();
+        toDo.Add(room);
         StartCoroutine(GenerateDungeon());
     }
 
     public IEnumerator GenerateDungeon()
     {
-        //run your split code and add the result to generatedRooms
-        //generatedRooms.Clear();
-        //int randomRoomSizeheight = Random.Range(1, 5);
-
-
+        yield return new WaitUntil(SpacePress);
         while (toDo.Count > 0)
         {
 
             RectInt newRoom = toDo[0];
             RectInt newOtherRoom = toDo[0];
 
-            if (toDo[0].width > expectedRoomSize && toDo[0].height < expectedRoomSize)
+            if (toDo[0].width > MaxRoomSize && toDo[0].height < MaxRoomSize)
                 widthSplit = true;
-            else if (toDo[0].height > expectedRoomSize && toDo[0].width < expectedRoomSize)
+            else if (toDo[0].height > MaxRoomSize && toDo[0].width < MaxRoomSize)
                 widthSplit = false;
 
             if (widthSplit)
@@ -57,23 +68,13 @@ public class DungeonGenerator : MonoBehaviour
 
                 // New room changes
                 newRoom.width = randomRoomSize;
-                //newRoom.height /= randomRoomSizeheight;
-
 
                 // Other new room changes
-                //Width
                 newOtherRoom.xMin = newRoom.width + newRoom.xMin;
                 newOtherRoom.width = toDo[0].width - newRoom.width;
-                //Height
-                //newOtherRoom.yMin = newRoom.height + newRoom.yMin;
-                //newOtherRoom.height = toDo[0].height - newRoom.height;
 
-                //Debug.Log(room);
-                //Debug.Log(newRoom.width);
-                //Debug.Log(newOtherRoom.width);
-
-                newRoom.width += 1;
-                newOtherRoom.xMin -= 1;
+                newRoom.width += roomOverlap;
+                //newOtherRoom.xMin -= 1;
             }
             else
             {
@@ -87,20 +88,20 @@ public class DungeonGenerator : MonoBehaviour
                 newOtherRoom.yMin = newRoom.height + newRoom.yMin;
                 newOtherRoom.height = toDo[0].height - newRoom.height;
 
-                newRoom.height += 1;
-                newOtherRoom.yMin -= 1;
+                newRoom.height += roomOverlap;
+                //newOtherRoom.yMin -= 1;
             }
 
-            generatedRooms.Add(newRoom);
-            generatedRooms.Add(newOtherRoom);
+
+            generatedRooms.Insert(0, newOtherRoom);
+            generatedRooms.Insert(0, newRoom);
 
             toDo.Remove(toDo[0]);
             foreach (var ro in generatedRooms)
             {
-                //Make it so that it will also keep going if one of the value is more than expected but only in that direction(width/height)
-                if (ro.width > expectedRoomSize || ro.height > expectedRoomSize)
+                if (ro.width > MaxRoomSize || ro.height > MaxRoomSize)
                 {
-                    toDo.Add(ro);
+                    toDo.Insert(0, ro);
                 }
             }
             foreach (var ro in toDo)
@@ -112,14 +113,54 @@ public class DungeonGenerator : MonoBehaviour
 
             yield return new WaitForSeconds(0.1f);
         }
+        yield return new WaitUntil(SpacePress);
+        StartCoroutine(GenerateDoors());
+    }
+
+    public IEnumerator GenerateDoors()
+    {
+        for (int i = 0; i < generatedRooms.Count; i++)
+        {
+            for (int j = i + 1; j < generatedRooms.Count; j++)
+            {
+                if (AlgorithmsUtils.Intersects(generatedRooms[i], generatedRooms[j]))
+                {
+
+                    RectInt door = AlgorithmsUtils.Intersect(generatedRooms[i], generatedRooms[j]);
+                    if (door.width < minRoomSize && door.height < minRoomSize)    continue;     // Checks if the intersect is to small to be a room and there for is a corner
+
+                    if (door.width == roomOverlap)
+                    {
+                        int randomDoorSpawn = Random.Range(door.yMin + doorOffset, door.yMax - doorOffset);
+                        door.yMin = (randomDoorSpawn);
+                        door.height = doorWidth;
+                    }
+                    if (door.height == roomOverlap)
+                    {
+                        int randomDoorSpawn = Random.Range(door.xMin + doorOffset, door.xMax - doorOffset);
+                        door.xMin = (randomDoorSpawn);
+                        door.width = doorWidth;
+                    }
+                    doors.Add(door);
+                    yield return new WaitForSeconds(0.1f);
+                }                
+            }
+        }
     }
 
 
     private void OnDrawGizmos()
     {
-        foreach (var ro in generatedRooms) AlgorithmsUtils.DebugRectInt(ro, Color.yellow);
-        foreach (var ro in toDo) AlgorithmsUtils.DebugRectInt(ro, Color.green);
+        foreach (var ro in generatedRooms) AlgorithmsUtils.DebugRectInt(ro, Color.blue);
+        foreach (var ro in toDo) AlgorithmsUtils.DebugRectInt(ro, Color.red);
+        foreach (var door in doors) AlgorithmsUtils.DebugRectInt(door, Color.green);
     }
 
-
+    public bool SpacePress()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            return true;
+        else
+            return false;
+    }
 }
